25ab567809e4dea4e2b3ecc57a59b0b63613efc9
Darin Williams <darinwilliams@darins-macbook-pro.local>
2017-09-27 01:25:12 -0500

    view data

--- bInformTests/bInformTests.swift
+++ bInformTests/bInformTests.swift
@@ -11,6 +11,8 @@ import XCTest
 
 class bInformTests: XCTestCase {
     
+    //this will be my first test
+    
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
