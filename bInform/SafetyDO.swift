6c72e0683554e7fe8c0b602cd48626ea193f83f3
Darin Williams <darinwilliams@darins-macbook-pro.local>
2017-10-03 23:55:08 -0400

    Initial Commit

---
+++ bInform/SafetyDO.swift
@@ -0,0 +1,33 @@
+//
+//  SafetyDO.swift
+//  bInform
+//
+//  Created by Darin Williams on 10/1/17.
+//  Copyright © 2017 dwilliams. All rights reserved.
+//
+
+import UIKit
+
+class SafetyDO{
+    
+    var desc: String
+    var manuName: String
+    var photo: UIImage?
+    
+    init?(desc: String, manuName: String, photo: UIImage?){
+        
+        //if description or manufactureName is empty fail initialization
+        if desc.isEmpty || manuName.isEmpty{
+            return nil
+        }
+        
+        //Stored properties
+        self.desc = desc
+        self.manuName = manuName
+        self.photo = photo
+        
+    }
+    
+    
+    
+}
